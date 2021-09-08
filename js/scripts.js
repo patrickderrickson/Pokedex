5c8f58213a861aa7beb60a142d3e1255064cb66d
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2021-09-08 13:17:35 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+alert('Hello World');
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
