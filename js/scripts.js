87866f1c90e03a48b1fa5b2d0841d42434ba4e30
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2021-09-08 13:22:28 -0700

    Linked CSS and added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 alert('Hello World');
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
+
+let favoriteFood = 'Sushi';
+document.write(favoriteFood);
