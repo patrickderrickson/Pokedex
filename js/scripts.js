06a5464f47305b710867ea7131c5cf73aa74d559
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2021-09-10 12:39:48 -0700

    Added pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', types: ['Grass', 'Poison'], height: '0.7m', weight: '6.9kg'},
+    {name: 'Ivysaur', types: ['Grass', 'Poison'], height: '1m', weight: '13kg'},
+    {name: 'Venusaur', types: ['Grass', 'Poison'], height: '2m', weight: '100kg'}
+];
