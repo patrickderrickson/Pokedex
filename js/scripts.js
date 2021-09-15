7c80ec417f9bf6cf3ddbd764aff1323516dfca0f
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2021-09-14 18:22:00 -0700

    Created loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+
 let pokemonList = [
-    {name: 'Bulbasaur', types: ['Grass', 'Poison'], height: '0.7m', weight: '6.9kg'},
-    {name: 'Ivysaur', types: ['Grass', 'Poison'], height: '1m', weight: '13kg'},
-    {name: 'Venusaur', types: ['Grass', 'Poison'], height: '2m', weight: '100kg'}
+    {name: 'Bulbasaur', types: ['Grass', 'Poison'], height: 0.7, weight: '6.9kg'},
+    {name: 'Ivysaur', types: ['Grass', 'Poison'], height: 1, weight: '13kg'},
+    {name: 'Venusaur', types: ['Grass', 'Poison'], height: 2, weight: '100kg'}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonName = pokemonList[i].name;
+    let pokemonHeight = pokemonList[i].height;
+
+    if (pokemonList[i].height >= 2) {
+        document.write( '<p>' + pokemonName + ' ' + '(Height: ' + pokemonHeight +  ')' + ' - Wow that\'s big!' + '</p>')  
+    }
+    else document.write( '<p>' + pokemonName + ' ' + '(Height: ' + + pokemonHeight +  ')' + '</p>')
+}
